b60ff9f43acb08ef50aaf2fae18210f3bebcccae
Goodestusername <ericdongcannn@hotmail.com>
2026-04-21 17:47:20 -0700

    Updated Android CapacitorGoogleMap.buildMarker to also support base64 data urls

--- plugin/android/src/main/java/com/capacitorjs/plugins/googlemaps/CapacitorGoogleMap.kt
+++ plugin/android/src/main/java/com/capacitorjs/plugins/googlemaps/CapacitorGoogleMap.kt
@@ -875,7 +875,17 @@ class CapacitorGoogleMap(
             } else {
                 try {
                     var stream: InputStream? = null
-                    if (marker.iconUrl!!.startsWith("https:")) {
+                    if (marker.iconUrl!!.startsWith("data:")) {
+                        // Extract the base64 part for the data URL
+                        val base64Data = marker.iconUrl!!.split(",")[1]
+
+                        // Decode the base64 string into a byte array
+                        val decodedBytes = Base64.decode(base64Data, Base64.DEFAULT)
+
+                        // Convert the byte array to an InputStream
+                        stream = ByteArrayInputStream(decodedBytes)
+                    }
+                    else if (marker.iconUrl!!.startsWith("https:")) {
                         stream = URL(marker.iconUrl).openConnection().getInputStream()
                     } else {
                         stream = this.delegate.context.assets.open("public/${marker.iconUrl}")
